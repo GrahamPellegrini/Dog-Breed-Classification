88a3264c7beba2b2d07256efdfd5a53d355fafc0
Graham Pellegrini <graham.pellegrini.22@um.edu.mt>
2025-02-17 13:21:21 +0100

    Trying to re push all

--- ssh/unreserved.sh
+++ ssh/unreserved.sh
@@ -5,14 +5,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=200M
-#SBATCH --time=0-10:00:00
-###SBATCH --reservation=cce3015
+#SBATCH --time=0-24:00:00
+##SBATCH --reservation=cce3015
 
 
 # job parameters
 #SBATCH --output=/opt/users/gpel0001/cce3207/ssh/out/cc3207_slurm_%A_%a.out
 #SBATCH --error=/opt/users/gpel0001/cce3207/ssh/err/cc3207_slurm_%A_%a.err
 #SBATCH --account=undergrad
+#SBATCH --job-name=vgg5
 
 # email user with progress
 #SBATCH --mail-user=graham.pellegrini.22@um.edu.mt
@@ -31,7 +32,7 @@ else
 fi
 
 # Define the path to the notebook you want to re-run
-NOTEBOOK_PATH="/opt/users/gpel0001/cce3207/Assignment/retry.ipynb"
+NOTEBOOK_PATH="/opt/users/gpel0001/cce3207/Assignment/dogidentifier.ipynb"
 
 # Re-run the notebook and save the output back to the same file
 jupyter nbconvert --execute --to notebook \
